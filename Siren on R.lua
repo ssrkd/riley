f1b931a920080554c6eefdaf20b6c1891522d198
srk451 <173401474+srkkd@users.noreply.github.com>
2026-05-20 20:38:44 +0500

    Clean up old files and remove Riley (40).lua

--- Siren on R.lua
+++
@@ -1,7 +0,0 @@
-function main()
-    while not isSampAvailable() do wait(0) end
-    while true do
-        wait(0)
-        if testCheat('r') and isCharInAnyCar(PLAYER_PED) then switchCarSiren(storeCarCharIsInNoSave(PLAYER_PED), not isCarSirenOn(storeCarCharIsInNoSave(PLAYER_PED))) end 
-    end
-end
