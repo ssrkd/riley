3ee31c0ff86a3a823fe8240cc8d69f322c7a0d0a
srk451 <173401474+srkkd@users.noreply.github.com>
2026-05-21 22:26:14 +0500

    Update to version 7.3 - Add local file fallback for roles, remove warning text

--- Riley.lua
+++ Riley.lua
@@ -7,7 +7,7 @@ local vkeys = require 'vkeys'
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
 
-local script_version = 7.2
+local script_version = 7.3
 local version_url = "https://raw.githubusercontent.com/ssrkd/riley/main/Rileyversion.json"
 local update_url = "https://raw.githubusercontent.com/ssrkd/riley/main/Riley.lua"
 
@@ -19,6 +19,9 @@ local supabase_service_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzd
 -- Кэш ролей пользователей
 local userRoles = {}
 local rolesLoaded = false
+
+-- Локальный файл с ролями как fallback
+local roles_file = getWorkingDirectory() .. "/config/roles.json"
 
 local showMenu = mimgui.new.bool(false)
 local showAct = mimgui.new.bool(false)
