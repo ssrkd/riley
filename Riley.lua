105e28eea141194d663165c860539b8c6fded1df
srk451 <173401474+srkkd@users.noreply.github.com>
2026-05-21 22:31:28 +0500

    Update to version 7.7 - Fix main function to call loadRolesFromFile on startup

--- Riley.lua
+++ Riley.lua
@@ -7,7 +7,7 @@ local vkeys = require 'vkeys'
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
 
-local script_version = 7.6
+local script_version = 7.7
 local version_url = "https://raw.githubusercontent.com/ssrkd/riley/main/Rileyversion.json"
 local update_url = "https://raw.githubusercontent.com/ssrkd/riley/main/Riley.lua"
 
